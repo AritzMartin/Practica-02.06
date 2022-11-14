121f1842eb5fc38a495f0f05d742b45732517136
amartingir <113341165+AritzMartin@users.noreply.github.com>
2022-11-14 10:06:51 +0100

    Ejercicio4 Terminado

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -6,3 +6,11 @@ print(diccionario)
 diccionario['edad'] = input('Cuantos años tienes?\n')
 print(diccionario)
 
+diccionario['sexo'] = input('Cual es tu género?\n')
+print(diccionario)
+
+diccionario['telefono'] = input('Introduce tu número de teléfono:\n')
+print(diccionario)
+
+diccionario['correo'] = input('Introduce tu dirección de correo:\n')
+print(diccionario)
