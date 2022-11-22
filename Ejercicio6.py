11c8cbb5c25cb72b8a0f1fcdb722bf02c46e64f5
amartingir <113341165+AritzMartin@users.noreply.github.com>
2022-11-22 12:40:45 +0100

    Ejercicio6 Sin terminar

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,7 +1,8 @@
 base_datos = []
+clase = {}
 
-print('********************************')
-print('          MENÚ                  ')
+print("********************************")
+print("             MENÚ                 ")
 print('  [1]. Añadir alumno/a          ')
 print('  [2]. Eliminar alumno/a        ')
 print('  [3]. Mostrar alumno/a         ')
@@ -10,13 +11,23 @@ print('  [5]. Listar alumnado aprobado ')
 print('  [6]. Terminar                 ')
 print('********************************')
 
-if input() == 1:
-    nif = input('Escribe el NIF del alunmo/a:\n')
-    nombre = input('Escribe el nombre del alunmo/a:\n')
-    apellidos = input('Escribe los apellidos del alunmo/a:\n')
-    telefono = input('Escribe el número de teléfono del alunmo/a:\n')
-    correo = input('Escribe el correo del alunmo/a:\n')
-    aprobado = input('El alunmo/a ha aprobado?\n')
-else:
-    print('ERROR')
+usr_option = int(input("Eliga una opcion :\n"))
+while True:
+    if usr_option == 1:
+        clase['nif'] = input('Escribe el NIF del alunmo/a:\n')
+        clase['nombre'] = input('Escribe el nombre del alunmo/a:\n')
+        clase['apellidos'] = input('Escribe los apellidos del alunmo/a:\n')
+        clase['telefono'] = input('Escribe el número de teléfono delalunmo/a:\n')
+        clase['correo'] = input('Escribe el correo del alunmo/a:\n')
+        clase['aprobado'] = input('El alunmo/a ha aprobado?\n')
+    if usr_option == 2:
+        nif = input('Escribe el NIF del alunmo/a:\n')
+        clase.keys()
+        if nif in clase.keys():
+            del clase
+
+    if usr_option == 6:
+        break
+    else:
+        print('ERROR')
 
